cd21b930f8f2aa48ad402567e4202be179eb5c5c
Saikat Kundu <saikatkundu712616@gmail.com>
2026-04-25 23:18:11 +0530

    first commit

---
+++ travel/models.py
@@ -0,0 +1,91 @@
+from django.db import models
+from django.contrib.auth.hashers import make_password, check_password
+
+# Create your models here.
+
+
+# User model
+class User(models.Model):
+    user_name = models.CharField(max_length=100)
+    user_email = models.EmailField(unique=True)
+    user_password = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.user_name
+    
+
+
+# Destination Model
+class Destination(models.Model):
+
+    name = models.CharField(max_length=100)
+    state = models.CharField(max_length=100)
+    description = models.TextField()
+    
+
+    def __str__(self):
+        return self.name
+    
+    
+
+
+# Hotel model
+class Hotel(models.Model):
+    destination = models.ForeignKey(Destination, on_delete=models.CASCADE)
+    hotel_name = models.TextField(max_length=100)
+    hotel_address = models.TextField()
+    ratings = models.FloatField()
+    price_per_night = models.DecimalField(max_digits=10, decimal_places=2)
+    room_availability = models.IntegerField()
+    image = models.ImageField(upload_to='hotel_image/')
+    populer_hotel=models.BooleanField(default=False)
+
+
+    def __str__(self):
+        return self.hotel_name
+    
+
+
+# package model
+class Package(models.Model):
+    destination = models.ForeignKey(Destination, on_delete=models.CASCADE)
+
+    hotel_name = models.ForeignKey(Hotel, on_delete=models.CASCADE)
+    package_name = models.CharField(max_length=100)
+    package_description = models.CharField()
+
+    package_price = models.FloatField()
+    duration_days = models.IntegerField()
+
+    image = models.ImageField(upload_to='package_image/')
+    populer_tour=models.BooleanField(default=False)
+
+
+    def __str__(self):
+        return self.package_name
+    
+
+
+# Booking Model
+
+class Booking(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    hotel = models.ForeignKey(Hotel, on_delete=models.CASCADE)
+
+    package = models.ForeignKey(Package, on_delete=models.CASCADE, null=True, blank=True)
+    name = models.CharField(max_length=100, default="Unknown")
+    email = models.EmailField(null=True, blank=True)
+    address = models.CharField(max_length=255, null=True, blank=True)
+    check_in = models.DateField(null=True, blank=True)
+    check_out = models.DateField()
+
+    guests = models.IntegerField()
+    total_price = models.DecimalField(max_digits=10, decimal_places=2)
+
+    booking_at = models.DateTimeField(auto_now_add=True)
+
+
+    def __str__(self):
+        return f"{self.user.user_name} booking"
+    
